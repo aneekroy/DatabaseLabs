456ed9e84193e4bdef0315cf95dae294159708e8
Aneek Roy <aneekroy@users.noreply.github.com>
2017-02-12 20:08:51 +0530

    Forms added for calculator

--- Calculator/Form1.Designer.vb
+++ Calculator/Form1.Designer.vb
@@ -22,9 +22,180 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 171)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "ADD"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(93, 171)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "SUBTRACT"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(174, 171)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "MULTIPLY"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(255, 171)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "DIVIDE"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(84, 52)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(221, 20)
+        Me.TextBox1.TabIndex = 4
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(84, 82)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(221, 20)
+        Me.TextBox2.TabIndex = 5
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(93, 213)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 6
+        Me.Button5.Text = "Reset"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(174, 213)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(75, 23)
+        Me.Button6.TabIndex = 7
+        Me.Button6.Text = "Exit"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label1.Font = New System.Drawing.Font("Arial Narrow", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(15, 52)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(52, 17)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Operand 1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label2.Font = New System.Drawing.Font("Arial Narrow", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(15, 82)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(52, 17)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Operand 2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(30, 122)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(37, 13)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Result"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(84, 119)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(221, 20)
+        Me.TextBox3.TabIndex = 11
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Arial Rounded MT Bold", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(86, 9)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(162, 33)
+        Me.Label4.TabIndex = 12
+        Me.Label4.Text = "Calculator"
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(344, 264)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Timer1 As Timer
 End Class
--- Calculator/Form1.vb
+++ Calculator/Form1.vb
@@ -1,3 +1,148 @@
 ﻿Public Class Form1
+    Dim FirstNumber As Decimal
+    Dim SecondNumber As Decimal
+    Dim Result As Decimal
+
+    Dim emptyTextBoxes =
+    From txt In Me.Controls.OfType(Of TextBox)()
+    Where txt.Text.Length = 0
+    Select txt.Name
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        '   emptyTextBoxes =
+        '   From txt In Me.Controls.OfType(Of TextBox)()
+        '   Where txt.Text.Length = 0
+        '   Select Case txt.Name
+
+        '   If emptyTextBoxes.Any Then
+        '   MessageBox.Show(String.Format("Please fill following textboxes: {0}",
+        '   String.Join(",", emptyTextBoxes)))
+        '   End If
+        'If String.IsNullOrEmpty(TextBox1.Text) & String.IsNullOrEmpty(TextBox2.Text) Then
+        If TextBox1.Text = Nothing And TextBox2.Text = Nothing Then
+            MessageBox.Show(String.Format("Please fill Operand 1 and Operand 2"))
+        ElseIf TextBox1.Text = Nothing Then
+            MessageBox.Show(String.Format("Please fill Operand 1 "))
+        ElseIf TextBox2.Text = Nothing Then
+            MessageBox.Show(String.Format("Please fill Operand 2 "))
+        Else
+            FirstNumber = TextBox1.Text
+            SecondNumber = TextBox2.Text
+            Result = FirstNumber + SecondNumber
+            TextBox3.Text = Result
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        '   If emptyTextBoxes.Any Then
+        '   MessageBox.Show(String.Format("Please fill following textboxes: {0}",
+        '   String.Join(",", emptyTextBoxes)))
+        '   End If
+        ' If String.IsNullOrEmpty(TextBox1.Text) & String.IsNullOrEmpty(TextBox2.Text) Then
+        'MessageBox.Show(String.Format("Please fill Operand 1 and Operand 2"))
+        'ElseIf String.IsNullOrEmpty(TextBox1.Text) Then
+        'MessageBox.Show(String.Format("Please fill Operand 1 "))
+        'ElseIf String.IsNullOrEmpty(TextBox2.Text) Then
+        'MessageBox.Show(String.Format("Please fill Operand 2 "))
+        If TextBox1.Text = Nothing And TextBox2.Text = Nothing Then
+            MessageBox.Show(String.Format("Please fill Operand 1 and Operand 2"))
+        ElseIf TextBox1.Text = Nothing Then
+            MessageBox.Show(String.Format("Please fill Operand 1 "))
+        ElseIf TextBox2.Text = Nothing Then
+            MessageBox.Show(String.Format("Please fill Operand 2 "))
+        Else
+            FirstNumber = TextBox1.Text
+        SecondNumber = TextBox2.Text
+        Result = FirstNumber - SecondNumber
+        TextBox3.Text = Result
+        End If
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        Close()
+    End Sub
+
+    ' Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    ' String todaysdate
+    'Me.Text = String.Format("{0:yyyy/MM/dd HH:mm}", DateTime.Now)
+    '= todaysdate
+    ' End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.Timer1.Start()
+    End Sub
+
+
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        '  If emptyTextBoxes.Any Then
+        ' MessageBox.Show(String.Format("Please fill following textboxes: {0}",
+        ' String.Join(",", emptyTextBoxes)))
+        ' End If
+        ' If String.IsNullOrEmpty(TextBox1.Text) & String.IsNullOrEmpty(TextBox2.Text) Then
+        'MessageBox.Show(String.Format("Please fill Operand 1 and Operand 2"))
+        'ElseIf String.IsNullOrEmpty(TextBox1.Text) Then
+        'MessageBox.Show(String.Format("Please fill Operand 1 "))
+        'ElseIf String.IsNullOrEmpty(TextBox2.Text) Then
+        'MessageBox.Show(String.Format("Please fill Operand 2 "))
+        'Else
+        If TextBox1.Text = Nothing And TextBox2.Text = Nothing Then
+            MessageBox.Show(String.Format("Please fill Operand 1 and Operand 2"))
+        ElseIf TextBox1.Text = Nothing Then
+            MessageBox.Show(String.Format("Please fill Operand 1 "))
+        ElseIf TextBox2.Text = Nothing Then
+            MessageBox.Show(String.Format("Please fill Operand 2 "))
+        Else
+            FirstNumber = TextBox1.Text
+            SecondNumber = TextBox2.Text
+            Result = FirstNumber * SecondNumber
+            TextBox3.Text = Result
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        ' If emptyTextBoxes.Any Then
+        ' MessageBox.Show(String.Format("Please fill following textboxes: {0}",
+        '   String.Join(",", emptyTextBoxes)))
+        'Else
+        ' If String.IsNullOrEmpty(TextBox1.Text) & String.IsNullOrEmpty(TextBox2.Text) Then
+        '  MessageBox.Show(String.Format("Please fill Operand 1 and Operand 2"))
+        ' ElseIf String.IsNullOrEmpty(TextBox1.Text) Then
+        'MessageBox.Show(String.Format("Please fill Operand 1 "))
+        'ElseIf String.IsNullOrEmpty(TextBox2.Text) Then
+        'MessageBox.Show(String.Format("Please fill Operand 2 "))
+        'Else
+        If TextBox1.Text = Nothing And TextBox2.Text = Nothing Then
+            MessageBox.Show(String.Format("Please fill Operand 1 and Operand 2"))
+        ElseIf TextBox1.Text = Nothing Then
+            MessageBox.Show(String.Format("Please fill Operand 1 "))
+        ElseIf TextBox2.Text = Nothing Then
+            MessageBox.Show(String.Format("Please fill Operand 2 "))
+        Else
+            FirstNumber = TextBox1.Text
+            SecondNumber = TextBox2.Text
+            If TextBox2.Text <> 0 Then
+                Result = FirstNumber / SecondNumber
+                TextBox3.Text = Result
+            Else
+                TextBox3.Text = "Error !"
+            End If
+        End If
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        TextBox1.Clear()
+        TextBox2.Clear()
+        TextBox3.Clear()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Me.Text = DateTime.Now.ToString("MM/dd/yyyy h:mm:ss tt")
+
+    End Sub
+
+
+
 
 End Class
